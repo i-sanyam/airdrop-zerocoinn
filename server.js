00737098a3e742f48fdf41b4cfac602b4ee1a8fd
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2023-09-17 17:54:03 +0530

    allow 100 per ip per 3 min

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 100, // limit each IP to 25 requests per windowMs
+  windowMs: 3 * 60 * 1000, // 3 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again later',
 });
 
