46c5a3d89d5c356a29d6a7477a04607adb4f0e7c
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2023-09-18 00:08:53 +0530

    tailwind made life easy

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 'use strict';
 
+const resetFieldsSetByApi = () => {
+	setTextContent('responseMessageText', '');
+	setTextContent('signatureUrl', '');
+	setTextContent('signatureText', '');
+};
+
 const handleSubmit = async (event) => {
 	event.preventDefault();
 	const form = document.getElementById('myForm');
 	const formData = new FormData(form);
 	const destinationAddress = formData.get('destination_address');
 	const amount = formData.get('amount');
-	hideElementById('signatureUrl');
-	hideElementById('signatureText');
-	hideElementById('successMessage');
-	hideElementById('errorMessage');
+	resetFieldsSetByApi();
 	try {
 		showLoader();
 		const response = await axios.get('/.netlify/functions/api/airdrop/ZEROCOINN', {
@@ -18,71 +21,44 @@ const handleSubmit = async (event) => {
 				destination_address: destinationAddress,
 			},
 		});
-		setDisplayFlex('successMessage');
-		setTextContent('successMessageText', 'Airdrop successful');
-		hideElementById('errorMessage');
-		setInnerHtml('signatureUrl', `<a href="${response.data.txUrl}" target="_blank">View transaction on Solana Explorer</a>`);
-		setTextContent('signatureText', `Signature: ${response.data.signature}`);
+		setUrl('signatureUrl', 'View transaction on Solana Explorer', response.data.txUrl);
+		setTextContent('signatureText', `Transaction Successful. Copy Signature for reference: ${response.data.signature}`);
 		hideLoader();
 	} catch (error) {
 		hideLoader();
-		setDisplayFlex('errorMessage');
-		setTextContent('errorMessageText', error.response.data.message);
-		hideElementById('signatureUrl');
-		hideElementById('signatureText');
-		hideElementById('successMessage');
+		setTextContent('responseMessageText', error.response.data.message);
 	}
+};
+
+const setUrl = (elementId, text, url) => {
+	const elem = document.getElementById(elementId);
+	elem.textContent = text;
+	elem.href = url;
 };
 
 const setTextContent = (elementId, text) => {
 	const elem = document.getElementById(elementId);
-	signatureText.style.display = 'flex';
+	// elem.style.display = 'flex';
 	elem.textContent = text;
-};
-
-const setDisplayFlex = (elementId) => {
-	const elem = document.getElementById(elementId);
-	elem.style.display = 'flex';
-};
-
-const setInnerHtml = (elementId, html) => {
-	const elem = document.getElementById(elementId);
-	elem.style.display = 'flex';
-	elem.innerHTML = html;
 };
 
 const handleReset = () => {
 	const form = document.getElementById('myForm');
 	form.reset();
-	hideElementById('signatureUrl');
-	hideElementById('successMessage');
-	hideElementById('errorMessage');
-	hideElementById('signatureText');
+	resetFieldsSetByApi();
 	hideLoader();
 };
 
-const hideElementById = (elementId) => {
-	const elem = document.getElementById(elementId);
-	elem.style.display = 'none';
-};
-
-const handleCloseError = () => {
-	hideElementById('errorMessage');
-};
-
-const handleCloseSuccess = () => {
-	hideElementById('successMessage');
-};
-
-const loader = document.getElementById('loader');
-const submitButton = document.getElementById('submitButton');
-
 const showLoader = () => {
+	const submitButton = document.getElementById('submitButton');
+	const loader = document.getElementById('loader');
 	submitButton.disabled = true;
 	loader.style.display = 'flex';
 };
 
 const hideLoader = () => {
+	const submitButton = document.getElementById('submitButton');
+	const loader = document.getElementById('loader');
 	submitButton.disabled = false;
 	loader.style.display = 'none';
 };
