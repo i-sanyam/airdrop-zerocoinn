213a366f110ecbdd4c9f9e970a4042e8248577df
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2023-09-17 17:23:12 +0530

    host src

--- server.js
+++ server.js
@@ -19,13 +19,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(500).send('Something broke!');
 };
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
-});
-
-app.get('/images/favicon.ico', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/images/favicon.ico'));
-});
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/api/airdrop/ZEROCOINN', async (req, res) => {
   try {
