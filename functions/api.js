a7001b7c8b24e4e9f253e80eec10e1e0ba02b1f6
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2023-09-17 20:30:08 +0530

    ratelimit managed by netlify

--- functions/api.js
+++ functions/api.js
@@ -2,21 +2,11 @@ const express = require('express');
 const { Router, } = require('express');
 const serverless = require('serverless-http');
 
-const rateLimit = require('express-rate-limit');
-
 const router = Router();
-
-const limiter = rateLimit({
-  windowMs: 3 * 60 * 1000, // 3 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later',
-});
 
 const { sendTokens } = require('../sol-helper');
 
 const app = express();
-
-app.use(limiter);
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
@@ -41,10 +31,6 @@ router.get('/airdrop/ZEROCOINN', async (req, res) => {
 app.use(errorHandler);
 
 app.use('/api/', router);
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
 
 app.use('/.netlify/functions/api', router);
 
